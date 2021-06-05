dffac7f24f1741cf0366264a697519eff16f4ab4
sakshu2810 <66720465+sakshu2810@users.noreply.github.com>
2021-06-05 17:57:57 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+          return (
+                   <div className="note">
+                     <h1> Javascript and React.js</h1>
+                     <p>This is sakshi markad an amazing workshop by Shaurya sinha was covered everything from scratch including javascript, react.js, html.</p>
+                   </div>
+
+          );
+}
+export default Note;
